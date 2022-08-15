843d5793edfeb5d0235e256f3bc5a53f6010b1c0
theHTMLprogrammer <stgupta9935@gmail.com>
2022-08-15 11:24:25 +0530

    Added some recursion programs of java

--- Java/TestProblems.java
+++ Java/TestProblems.java
@@ -1,23 +1,21 @@
 import java.util.*;
 
 public class TestProblems {
-    public static void main(String[] args) {
-        try (Scanner sc = new Scanner(System.in)) {
-            System.out.println("Enter the size of the array: ");
-            int size = sc.nextInt(), i;
-            System.out.println("Enter the elements of the array: ");
-            int names[] = new int[size];
-            for (i = 0; i < names.length; i++) {
-                names[i] = sc.nextInt();
-            }
-            for (i = 0; i < names.length - 1; i++) {
-                if (names[i] > names[i + 1]) {
-                    System.out.println("Not in ascending order!!!");
-                    return;
-                }
-            }
-            if (i == size - 1)
-                System.out.println("The array is in ascending order!!!");
+    public static void checkAscending(int[] arr, int idx) {
+        if (idx == arr.length) {
+            System.out.println("Ascending!");
+            return;
         }
+        if (arr[idx] >= arr[idx - 1]) {
+            checkAscending(arr, idx + 1);
+        } else {
+            System.out.println("Not Ascending!");
+            return;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 12, 34, 6, 68 };
+        checkAscending(arr, 1);
     }
 }
