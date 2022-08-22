946f83e629c757789d9d33a10c19941790fd39f0
theHTMLprogrammer <stgupta9935@gmail.com>
2022-08-23 00:33:24 +0530

    Add Recursion and Backtracking problems in java

--- Java/TestProblems.java
+++ Java/TestProblems.java
@@ -1,21 +1,3 @@
-import java.util.*;
-
 public class TestProblems {
-    public static void checkAscending(int[] arr, int idx) {
-        if (idx == arr.length) {
-            System.out.println("Ascending!");
-            return;
-        }
-        if (arr[idx] >= arr[idx - 1]) {
-            checkAscending(arr, idx + 1);
-        } else {
-            System.out.println("Not Ascending!");
-            return;
-        }
-    }
 
-    public static void main(String[] args) {
-        int[] arr = { 12, 34, 6, 68 };
-        checkAscending(arr, 1);
-    }
 }
